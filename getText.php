0fce243f80283e9b4b370280e33033f83f3dc8e5
jkrek17 <jkrek17@gmail.com>
2026-01-28 14:18:36 -0500

    Create getText.php

---
+++ getText.php
@@ -0,0 +1,12 @@
+<?php
+
+$urls = array("https://ocean.weather.gov/shtml/NFDOFFNT1.txt", "https://ocean.weather.gov/shtml/NFDOFFNT2.txt", "https://ocean.weather.gov/shtml/NFDOFFPZ5.txt","https://ocean.weather.gov/shtml/NFDOFFPZ6.txt");
+
+$prods = array("NT1", "NT2", "PZ5", "PZ6");
+
+$NT1_zones = array("ANZ800", "ANZ805", "ANZ900", "ANZ810", "ANZ815");
+$NT2_zones = array("ANZ820", "ANZ915", "ANZ920", "ANZ905", "ANZ910", "ANZ825", "ANZ828", "ANZ925", "ANZ830", "ANZ833", "ANZ930", "ANZ835", "ANZ935");
+$PZ5_zones = array("PZZ800", "PZZ900", "PZZ805", "PZZ905", "PZZ810", "PZZ910", "PZZ815", "PZZ915");
+$PZ6_zones = array("PZZ820", "PZZ920", "PZZ825", "PZZ925", "PZZ830", "PZZ930", "PZZ835", "PZZ935", "PZZ840", "PZZ940","PZZ945");
+
+?>
